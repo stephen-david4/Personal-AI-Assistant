af4412a1db4e0191ab6c9930313907da0caaa7a6
stephen-david4 <stephendavid.aiml@gmail.com>
2026-06-05 18:17:02 +0530

    Update rag_system.py

--- rag_system.py
+++ rag_system.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.vectorstores import Chroma
 from groq import Groq
 import streamlit as st
